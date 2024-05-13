392234867130c519b1b7dde8bc498817b7b7b66c
codefug <robot9917@naver.com>
2024-05-13 16:26:59 +0900

    edit cicd file, test cache action

--- cicd/src/App.jsx
+++ cicd/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage((prevPage) => prevPage + 1);
+  }, []);
 
   return (
     <>
@@ -16,7 +21,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{page}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
